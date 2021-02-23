4b7d83d7a9b4083003fa606542e6786c5f75a41b
Anthony Gégo <anthonygego@hotmail.com>
2021-02-23 15:03:11 +0100

    Flask migration

--- lti_resize_frame/__init__.py
+++ lti_resize_frame/__init__.py
@@ -5,29 +5,21 @@
 
 """ Posts the iframe size in an page for an LTI course """
 import os
-import web
 
+from flask import send_from_directory
+from inginious.frontend.pages.utils import INGIniousPage
 
 __version__ = "0.1.michelfra"
 PATH_TO_PLUGIN = os.path.abspath(os.path.dirname(__file__))
 
-class StaticMockPage(object):
-    # TODO: Replace by shared static middleware and let webserver serve the files
+class StaticMockPage(INGIniousPage):
     def GET(self, path):
-        if not os.path.abspath(PATH_TO_PLUGIN) in os.path.abspath(os.path.join(PATH_TO_PLUGIN, path)):
-            raise web.notfound()
-
-        try:
-            with open(os.path.join(PATH_TO_PLUGIN, "static", path), 'rb') as file:
-                return file.read()
-        except:
-            raise web.notfound()
+        return send_from_directory(os.path.join(_dir_path, "static"), path)
 
     def POST(self, path):
         return self.GET(path)
 
-
 def init(plugin_manager, _, _2, _3):
     """ Init the plugin """
-    plugin_manager.add_page('/plugins/lti_resize_iframe/static/(.+)', StaticMockPage)
+    plugin_manager.add_page('/plugins/lti_resize_iframe/static/<path:path>', StaticMockPage.as_view("ltiresizeframestaticpage"))
     plugin_manager.add_hook("javascript_footer", lambda: "/plugins/lti_resize_iframe/static/lti_resize_iframe.js")
